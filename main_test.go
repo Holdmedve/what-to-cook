7cecebb7e2cd65b682c9f4cd82ccd643a73bf03c
Holdmedve <talos.boti@gmail.com>
2025-04-21 15:17:52 +0200

    Streamline test helpers

--- main_test.go
+++ main_test.go
@@ -3,21 +3,16 @@ package main
 import (
 	"log"
 	"os"
-	"strings"
 	"testing"
 )
 
 const recipeFilePath = "test.txt"
 
 func TestSubmittedRecipeIsSaved(t *testing.T) {
-	setupRecipeFile(recipeFilePath)
-	page := setupPage()
-	_, err := page.Goto("127.0.0.1:80")
-	assertErrorToNilf("could not goto: %w", err)
-	fillPage(page, "[name='title']", "test-recipe-name")
-	fillPage(page, "[name='description']", "test-recipe-description")
+	page := setup(recipeFilePath)
+	defer teardown(page, recipeFilePath)
 
-	clickPage(page, "[name='submit']")
+	saveRecipe(page, "test-recipe-name", "test-recipe-description")
 
 	b, err := os.ReadFile(recipeFilePath)
 	if err != nil {
@@ -31,58 +26,23 @@ func TestSubmittedRecipeIsSaved(t *testing.T) {
 }
 
 func Test_Submit2Recipes_BothAreDisplayed(t *testing.T) {
-	setupRecipeFile(recipeFilePath)
-	page := setupPage()
-	_, err := page.Goto("127.0.0.1:80")
-	assertErrorToNilf("could not goto: %w", err)
+	page := setup(recipeFilePath)
+	defer teardown(page, recipeFilePath)
 	testRecipeTitleA := "testRecipeTitleA"
 	testRecipeDescriptionA := "testRecipeDescriptioncA"
 	testRecipeTitleB := "testRecipeTitleB"
 	testRecipeDescriptionB := "testRecipeDescriptioncB"
 
-	fillPage(page, "[name='title']", testRecipeTitleA)
-	fillPage(page, "[name='description']", testRecipeDescriptionA)
-	clickPage(page, "[name='submit']")
+	saveRecipe(page, testRecipeTitleA, testRecipeDescriptionA)
+	saveRecipe(page, testRecipeTitleB, testRecipeDescriptionB)
 
-	fillPage(page, "[name='title']", testRecipeTitleB)
-	fillPage(page, "[name='description']", testRecipeDescriptionB)
-	clickPage(page, "[name='submit']")
-
-	titleLocator, err := page.Locator("[name='displayTitle']").All()
-	if len(titleLocator) != 2 {
-		log.Fatalf("there should be exactly 2 titles, found: %d", len(titleLocator))
-	}
-
-	actualRecipeTitleA, _ := titleLocator[0].TextContent()
-	actualRecipeTitleB, _ := titleLocator[1].TextContent()
-	if strings.Contains(actualRecipeTitleA, testRecipeTitleA) == false {
-		log.Fatalf("expected: %s, to contain: %s", actualRecipeTitleA, testRecipeTitleA)
-	}
-	if strings.Contains(actualRecipeTitleB, testRecipeTitleB) == false {
-		log.Fatalf("expected: %s, to contain: %s", actualRecipeTitleB, testRecipeTitleB)
-	}
-
-	descriptionLocator, err := page.Locator("[name='displayDescription']").All()
-	if len(descriptionLocator) != 2 {
-		log.Fatalf("there should be exactly 2 titles, found: %d", len(descriptionLocator))
-	}
-
-	actualRecipeDescriptionA, _ := descriptionLocator[0].TextContent()
-	actualRecipeDescriptionB, _ := descriptionLocator[1].TextContent()
-	if strings.Contains(actualRecipeDescriptionA, testRecipeDescriptionA) == false {
-		log.Fatalf("expected: %s, to contain: %s", actualRecipeDescriptionA, testRecipeDescriptionA)
-	}
-	if strings.Contains(actualRecipeDescriptionB, testRecipeDescriptionB) == false {
-		log.Fatalf("expected: %s, to contain: %s", actualRecipeDescriptionB, testRecipeDescriptionB)
-	}
+	assertRecipeIsDisplayed(page, testRecipeTitleA, testRecipeDescriptionA)
+	assertRecipeIsDisplayed(page, testRecipeTitleB, testRecipeDescriptionB)
 }
 
 func Test_Submit3RecipesDelete2nd_1stAnd3rdAreDisplayed(t *testing.T) {
-	setupRecipeFile(recipeFilePath)
-	page := setupPage()
-	_, err := page.Goto("127.0.0.1:80")
-	assertErrorToNilf("could not goto: %w", err)
-
+	page := setup(recipeFilePath)
+	defer teardown(page, recipeFilePath)
 	testRecipeTitleA := "testRecipeTitleA"
 	testRecipeDescriptionA := "testRecipeDescriptioncA"
 	testRecipeTitleB := "testRecipeTitleB"
@@ -90,18 +50,9 @@ func Test_Submit3RecipesDelete2nd_1stAnd3rdAreDisplayed(t *testing.T) {
 	testRecipeTitleC := "testRecipeTitleC"
 	testRecipeDescriptionC := "testRecipeDescriptioncC"
 
-	fillPage(page, "[name='title']", testRecipeTitleA)
-	fillPage(page, "[name='description']", testRecipeDescriptionA)
-	clickPage(page, "[name='submit']")
-
-	fillPage(page, "[name='title']", testRecipeTitleB)
-	fillPage(page, "[name='description']", testRecipeDescriptionB)
-	clickPage(page, "[name='submit']")
-
-	fillPage(page, "[name='title']", testRecipeTitleC)
-	fillPage(page, "[name='description']", testRecipeDescriptionC)
-	clickPage(page, "[name='submit']")
-
+	saveRecipe(page, testRecipeTitleA, testRecipeDescriptionA)
+	saveRecipe(page, testRecipeTitleB, testRecipeDescriptionB)
+	saveRecipe(page, testRecipeTitleC, testRecipeDescriptionC)
 	deleteBtns, err := page.Locator("[name='deleteBtn']").All()
 	assertErrorToNilf("could not locate and fill: %w", err)
 	deleteBtns[1].Click()
@@ -110,27 +61,6 @@ func Test_Submit3RecipesDelete2nd_1stAnd3rdAreDisplayed(t *testing.T) {
 	if len(titleLocator) != 2 {
 		log.Fatalf("there should be exactly 2 titles, found: %d", len(titleLocator))
 	}
-
-	firstRecipeTitleA, _ := titleLocator[0].TextContent()
-	secondRecipeTitleC, _ := titleLocator[1].TextContent()
-	if strings.Contains(firstRecipeTitleA, testRecipeTitleA) == false {
-		log.Fatalf("expected: %s, to contain: %s", firstRecipeTitleA, testRecipeTitleA)
-	}
-	if strings.Contains(secondRecipeTitleC, testRecipeTitleC) == false {
-		log.Fatalf("expected: %s, to contain: %s", secondRecipeTitleC, testRecipeTitleC)
-	}
-
-	descriptionLocator, err := page.Locator("[name='displayDescription']").All()
-	if len(descriptionLocator) != 2 {
-		log.Fatalf("there should be exactly 2 titles, found: %d", len(descriptionLocator))
-	}
-
-	firstRecipeDescriptionA, _ := descriptionLocator[0].TextContent()
-	secondRecipeDescriptionC, _ := descriptionLocator[1].TextContent()
-	if strings.Contains(firstRecipeDescriptionA, testRecipeDescriptionA) == false {
-		log.Fatalf("expected: %s, to contain: %s", firstRecipeDescriptionA, testRecipeDescriptionA)
-	}
-	if strings.Contains(secondRecipeDescriptionC, testRecipeDescriptionC) == false {
-		log.Fatalf("expected: %s, to contain: %s", secondRecipeDescriptionC, testRecipeDescriptionC)
-	}
+	assertRecipeIsDisplayed(page, testRecipeTitleA, testRecipeDescriptionA)
+	assertRecipeIsDisplayed(page, testRecipeTitleC, testRecipeDescriptionC)
 }
---
+++ testHelpers.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"log"
+	"os"
+	"slices"
+	"strings"
+
+	"github.com/playwright-community/playwright-go"
+)
+
+func assertErrorToNilf(message string, err error) {
+	if err != nil {
+		log.Fatalf(message, err)
+	}
+}
+
+func fillPage(page playwright.Page, selector string, value string) {
+	err := page.Locator(selector).Fill(value)
+	if err != nil {
+		log.Fatalf("error during fill using selector %s and value %s: %s", selector, value, err.Error())
+	}
+}
+
+func clickPage(page playwright.Page, selector string) {
+	err := page.Locator(selector).Click()
+	if err != nil {
+		log.Fatalf("error during click using selector %s: %s", selector, err.Error())
+	}
+}
+
+func setupPage() playwright.Page {
+	pw, err := playwright.Run()
+	assertErrorToNilf("could not launch playwright: %w", err)
+	browser, err := pw.Chromium.Launch(playwright.BrowserTypeLaunchOptions{
+		Headless: playwright.Bool(false),
+	})
+	assertErrorToNilf("could not launch Chromium: %w", err)
+
+	context, err := browser.NewContext()
+	assertErrorToNilf("could not create context: %w", err)
+	page, err := context.NewPage()
+	assertErrorToNilf("could not create page: %w", err)
+
+	return page
+}
+
+func setupRecipeFile(filePath string) {
+	file, err := os.Create(filePath)
+	defer file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func saveRecipe(page playwright.Page, title string, descr string) {
+	fillPage(page, "[name='title']", title)
+	fillPage(page, "[name='description']", descr)
+	clickPage(page, "[name='submit']")
+}
+
+func setup(recipeFilePath string) playwright.Page {
+	setupRecipeFile(recipeFilePath)
+	page := setupPage()
+	_, err := page.Goto("127.0.0.1:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return page
+}
+
+func teardown(page playwright.Page, recipeFilePath string) {
+	page.Close()
+	setupRecipeFile(recipeFilePath)
+}
+
+func assertRecipeIsDisplayed(page playwright.Page, expectedTitle string, expectedDescr string) {
+	titleLocator, _ := page.Locator("[name='displayTitle']").All()
+	actualTitles := make([]string, 0)
+	for _, title := range titleLocator {
+		t, _ := title.TextContent()
+		actualTitles = append(actualTitles, t)
+	}
+	containsTitle := slices.ContainsFunc(actualTitles, func(s string) bool {
+		return strings.Contains(s, expectedTitle)
+	})
+	if containsTitle == false {
+		log.Fatalf("expected title %s was not found", expectedTitle)
+	}
+
+	descrLocator, _ := page.Locator("[name='displayDescription']").All()
+	actualDescriptions := make([]string, 0)
+	for _, descr := range descrLocator {
+		d, _ := descr.TextContent()
+		actualDescriptions = append(actualDescriptions, d)
+	}
+	containsDescr := slices.ContainsFunc(actualDescriptions, func(s string) bool {
+		return strings.Contains(s, expectedDescr)
+	})
+	if containsDescr == false {
+		log.Fatalf("expected description %s was not found", expectedDescr)
+	}
+}
